b48710d9225fd292cd89592642d63b394356272f
Nicolás Cardiel <cardiel@ucm.es>
2017-06-23 16:40:03 +0200

    Include imath.py.

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ def setup_package():
         entry_points={
             'console_scripts': [
                 'r6-addnf = rsix.addnf:main',
+                'r6-imath = rsix.imath:main',
                 'r6-imcombine = rsix.imcombine:main',
                 'r6-insert_keyword_in_FITS = rsix.insert_keyword_in_FITS:main'
             ],
