8fe8d8a8af0cd4686ba0d5af819e5070b535d404
Nicolás Cardiel <cardiel@ucm.es>
2017-06-27 22:42:48 +0100

    Include cosinebell.py function.

---
+++ rsix/cosinebell.py
@@ -0,0 +1,21 @@
+def cosinebell(n, fraction):
+    """Return a cosine bell spanning n pixels, masking a fraction of pixels
+    
+    Parameters
+    ----------
+    n : int
+        Number of pixels.
+    fraction : float
+        Length fraction over which the data will be masked.
+    
+    """
+    
+    mask = np.ones(n)
+    nmasked = int(fraction * n)
+    for i in range(nmasked):
+        yval = 0.5 * (1 - np.cos(np.pi * float(i) / float(nmasked)))
+        mask[i] = yval
+        mask[n - i - 1] = yval
+        
+    return mask
+
