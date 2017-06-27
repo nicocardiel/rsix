09d78a2bc22a9409e4f4129b873d748de43b4aab
Nicolás Cardiel <cardiel@ucm.es>
2017-06-27 01:05:24 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -18,7 +18,9 @@ def setup_package():
                 'r6-imath = rsix.imath:main',
                 'r6-imcombine = rsix.imcombine:main',
                 'r6-insert_keyword_in_FITS = rsix.insert_keyword_in_FITS:main',
-                'r6-replace_image = rsix.replace_image:main'
+                'r6-replace_image = rsix.replace_image:main',
+                'r6-subsets_of_fileinfo_from_txt = ' +
+                    'rsix.subsets_of_fileinfo_from_txt:main'
             ],
             },
         setup_requires=['rsix'],
